388acff002b50722518b4cf7db405c80bf034ccb
Ryan <pv.ryan14@gmail.com>
2018-02-20 15:15:37 -0500

    Initial project setup

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,32 @@
+@file:Suppress("unused")
+
+/*
+* Copyright 2018 Priyank Vasa
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+internal object Dependencies {
+    const val buildGradle = "com.android.tools.build:gradle:${Versions.buildGradle}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
+    const val appcompatV7 = "com.android.support:appcompat-v7:${Versions.support}"
+    const val supportDesign = "com.android.support:design:${Versions.support}"
+}
+
+internal object TestDependencies {
+    const val junit = "junit:junit:${Versions.junit}"
+}
+
+internal object AndroidTestDependencies {
+    const val runner = "com.android.support.test:runner:${Versions.runner}"
+    const val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+}
