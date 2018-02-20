11b72512f69b81b5e58bb480c00b8d41e43648c4
Priyank Vasa <pv.ryan14@gmail.com>
2018-02-20 15:15:37 -0500

    Initial project setup

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Priyank Vasa
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@Suppress("unused")
+internal object Versions {
+
+    // App and SDK versions
+    const val vCode = 100
+    const val vName = "1.0.0"
+    const val compileSdk = 27
+    const val minSdk = 14
+    const val targetSdk = 27
+
+    // Dependency versions
+    const val buildGradle = "3.0.1"
+    const val kotlin = "1.2.21"
+    const val support = "27.0.2"
+
+    // Test dependency versions
+    const val junit = "4.12"
+
+    // Android test dependency versions
+    const val runner = "1.0.1"
+    const val espresso = "3.0.1"
+}
